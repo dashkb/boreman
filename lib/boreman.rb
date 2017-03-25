66f6345ed96cccb8c6415c9d2a4238869eafffdd
Kyle Brett <kyle@kylebrett.com>
2017-03-24 22:38:35 -0700

    Syslog

--- lib/boreman.rb
+++ lib/boreman.rb
@@ -93,7 +93,7 @@ module Boreman
     if cmd = procs[selector]
       cmd = prepare_command(cmd)
 
-      pid = Process.spawn(cmd)
+      pid = Process.spawn(cmd, out: '/var/log/syslog', err: '/var/log/syslog')
       Process.detach(pid)
       write_pid selector, pid
       puts "Started #{selector}, pid = #{pid}"
